357780516ce5c3cda1afe9fb720396c8de16adf0
gayankrishanthan <dias.krishanthan@gmail.com>
2017-12-13 22:41:21 +0530

    Implement the initial viewModel classes for the view controllers.

---
+++ BBSLocation/Controllers/Loading/BBCLoadingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BBCLoadingViewModel.swift
+//  BBSLocation
+//
+//  Created by Gayan dias on 12/13/17.
+//  Copyright © 2017 Gayan dias. All rights reserved.
+//
+
+import Foundation
+
+class BBCLoadingViewModel {
+    
+}
---
+++ BBSLocation/Controllers/Map/BBCMapViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BBCMapViewModel.swift
+//  BBSLocation
+//
+//  Created by Gayan dias on 12/13/17.
+//  Copyright © 2017 Gayan dias. All rights reserved.
+//
+
+import Foundation
+
+class BBCMapViewModel {
+    
+}
